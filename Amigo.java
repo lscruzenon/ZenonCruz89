e8fd631375e1dbddfb7c89d9f8998b4e1dc4a4c2
lscruzenon <31417587+lscruzenon@users.noreply.github.com>
2017-08-29 14:30:32 -0500

    test2
    test2

---
+++ Amigo.java
@@ -0,0 +1,10 @@
+package amistad;
+
+public class Amigo {
+	
+	String nombre = "Carlos";
+	
+	public void saludo(){
+		System.out.println("Hola soy " + nombre);
+	}
+}
---
+++ Companero.java
@@ -0,0 +1,10 @@
+package companerismo;
+
+public class Companero{
+	
+	String nombre = "Jose";
+	
+	public void saludo(){
+		System.out.println("Hola compa "+nombre);
+	}
+}
